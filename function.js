ff0c84ef11f37d01687fe7080e5d5b43121457dc
Chris-PC <ckong6953@gmail.com>
2022-08-11 13:28:28 -0700

    File and style upload

---
+++ function.js
@@ -0,0 +1,40 @@
+var image_index = 0
+window.onload = function () {
+    renderButton()
+} 
+
+function renderButton(){
+    var height = document.querySelector('.mainaltcolumn').offsetHeight
+    document.getElementById("sidebutton").height = height
+}
+
+function fetchId(){
+    var current_id = "image_" + image_index
+    return document.getElementById(current_id)
+}
+
+function browseLeft() {
+    fetchId().style.display = "none"
+    switch(image_index){
+        case 0:
+            image_index = 11
+            break
+        default:
+            image_index--
+            break;
+    }
+    fetchId().style.display = "block"
+}
+
+function browseRight() {
+    fetchId().style.display = "none"
+    switch(image_index){
+        case 11:
+            image_index = 0
+            break
+        default:
+            image_index++
+            break;
+    }
+    fetchId().style.display = "block"
+}
